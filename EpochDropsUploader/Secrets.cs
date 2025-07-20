0ae2b01bf1f81dfc1ca616bdf7a0f5f97d275c09
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-07-20 20:38:57 +0200

    Empty Secrets boiler

---
+++ EpochDropsUploader/Secrets.cs
@@ -0,0 +1,8 @@
+namespace EpochDropsUploader
+{
+    public static class Secrets
+    {
+        public const string UploadKey = ""; // Ask owner for key
+        public const string AllowedRealm = "";
+    }
+}
