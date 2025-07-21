2b8e506428a130ec38e5b8d7f7014c2b86cf5ff9
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-07-21 21:06:40 +0200

    leaked key, changing lol

--- EpochDropsUploader/Secrets.cs
+++ EpochDropsUploader/Secrets.cs
@@ -2,7 +2,7 @@ namespace EpochDropsUploader
 {
     public static class Secrets
     {
-        public const string UploadKey = "Tycho7752";
-        public const string AllowedRealm = "ChromieCraft";
+        public const string UploadKey = "";
+        public const string AllowedRealm = "";
     }
 }
