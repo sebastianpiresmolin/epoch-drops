884caae2c88d9d4a7346862902f71cfc659c94ae
Sebastian Molin <piresmolinsecure@gmail.com>
2025-08-25 19:21:56 +0200

    Remove update notice

--- epoch_drops_web/app/page.tsx
+++ epoch_drops_web/app/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
           <h2 className="font-bold mt-2">Want to help gather data? Download the addon <a href="https://github.com/sebastianpiresmolin/epoch-drops/blob/main/README.md"
                                                                                          className="text-blue-400">here</a>
           </h2>
-          <h1 className="pt-6 text-xl text-green-500 font-bold">NEW WORKING ADDON 2025-08-17, please update on link above</h1>
       </div>
   );
 }
